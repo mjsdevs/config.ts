7e5a583647226b7a83ff7a03fb00a9368cea256d
Romildo Junior <jose.romildo.junior@concrete.com.br>
2021-06-12 00:04:26 -0300

    docs: add usage example

---
+++ src/test.ts
@@ -0,0 +1,27 @@
+import { Result, ConfigurationServiceFactory, ValidationFunction } from './index'
+
+class Env {
+	constructor(
+		public NODE_ENV: string,
+		public PORT: number
+	) { }
+}
+
+const handler: ValidationFunction<Env> = (env: NodeJS.ProcessEnv) => {
+	if (env['NODE_ENV'] && env['PORT']) {
+		return Result.ok(new Env(env['NODE_ENV'], Number(env['PORT'])));
+	}
+
+	return Result.fail<Env>('Erro na validação de alguma variavel. Verifique o .env');
+}
+
+const factory = new ConfigurationServiceFactory<Env>();
+const service = factory.create({
+	validation: {
+		handler,
+	},
+	options: {}
+});
+
+const env = service.get('NODE_ENV');
+console.log(env);
