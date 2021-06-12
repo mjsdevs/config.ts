eaf26e4d349fe12b3b23f82aea3f571e62545c03
Romildo Junior <jose.romildo.junior@concrete.com.br>
2021-06-11 22:03:57 -0300

    feat(service): add configuration service

---
+++ src/ConfigurationService.ts
@@ -0,0 +1,15 @@
+export class ConfigurationService<Environment extends Object> {
+	constructor(private readonly variables: Environment) { }
+
+	/**
+	 * Returns the value of some desired variable.
+	 * An optional type paramenter T can be passed to
+	 * convert the variable value (default: string).
+	 *  
+	 * @param key name of the variable
+	 * @returns Value of the variable
+	 */
+	public get<T = string>(key: keyof Environment) {
+		return <T><unknown>this.variables[key];
+	}
+}
