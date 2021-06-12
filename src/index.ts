323f6d7baaf25886be73b42cc415d3faa9f445f3
Romildo Junior <jose.romildo.junior@concrete.com.br>
2021-06-11 23:44:48 -0300

    fix: make ValidatioFunction type public

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ConfigurationService } from './ConfigurationService';
 export { ConfigurationServiceFactory } from './ConfigurationServiceFactory';
 export { Result } from './Result';
+export { ValidationFunction } from './ValidationFunction';
